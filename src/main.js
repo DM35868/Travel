dd6e1cd62612ddfe459f469c124988d2dd93374a
DM <578328962@qq.com>
2019-03-30 21:21:54 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/css/reset.css'
+// 引入后用于解决移动端出现1像素边框的问题
+import './assets/css/border.css'
 
 Vue.config.productionTip = false
 
