38c3ccc049eedee123d67fbfae41ec8056b140cf
DM <578328962@qq.com>
2019-04-16 22:14:18 +0800

    11

--- src/components/detail/Banner.vue
+++ src/components/detail/Banner.vue
@@ -1,48 +1,70 @@
 <template>
-    <div class="banner">
-        <img src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg" alt="" class="banner-img">
-        <div class="banner-info">
-            <div class="banner-title">323232</div>
-            <div class="banner-number"><span class="travel-iconfont icontupian banner-icon"></span>32</div>
+    <div class="banner-box">
+        <div class="banner" @click="showImgs">
+            <img src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg" alt="" class="banner-img">
+            <div class="banner-info">
+                <div class="banner-title">323232</div>
+                <div class="banner-number"><span class="travel-iconfont icontupian banner-icon"></span>32</div>
+            </div>
         </div>
+        <gallery v-show="showStatus" @close="closeImgs"></gallery>
     </div>
 </template>
 
 <script>
+import Gallery from './Gallery.vue'
 export default {
-    
+    name:'banner-box',
+    components:{
+        Gallery
+    },
+    data(){
+        return {
+            showStatus:false,
+        }
+    },
+    methods:{
+        showImgs(){
+            this.showStatus = true;
+        },
+        closeImgs(){
+            this.showStatus = false;
+        }
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
-    .banner{
-        position: relative;
-        overflow: hidden;
-        height:0;
-        padding-bottom:55%;
-        .banner-img{
-            width:100%;
-        }
-        .banner-info{
-            display:flex;
-            position:absolute;
-            left:0;
-            right:0;
-            bottom:0
-            color:#fff;
-            line-height:.6rem;
-            background-image: linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,.8));
-            .banner-title{
-                flex:1;
-                padding: 0 .2rem;
+    .banner-box{
+        .banner{
+            position: relative;
+            overflow: hidden;
+            height:0;
+            padding-bottom:55%;
+            .banner-img{
+                width:100%;
             }
-            .banner-number{
-                padding: 0 .4rem;
-                background-color: rgba(0,0,0,.3);
-                border-radius:.24rem;
-                .banner-icon{
-                    font-size: .24rem
-                    padding-right: .1rem;
+            .banner-info{
+                display:flex;
+                position:absolute;
+                left:0;
+                right:0;
+                bottom:0
+                color:#fff;
+                line-height:.6rem;
+                background-image: linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,.8));
+                .banner-title{
+                    flex:1;
+                    padding: 0 .2rem;
+                }
+                .banner-number{
+                    padding: 0 .4rem;
+                    background-color: rgba(0,0,0,.3);
+                    border-radius:.24rem;
+                    .banner-icon{
+                        font-size: .24rem
+                        padding-right: .1rem;
+                    }
                 }
             }
         }
---
+++ src/components/detail/Gallery.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="gallery" @click="closeImgs">
+       <div class="gallery-img">
+           <swiper :options="swiperOptions">
+                <swiper-slide v-for="(item,key) in imgs" :key=key>
+                    <img class="banner-img" :src="item">
+                </swiper-slide>
+                <div class="swiper-pagination" slot="pagination"></div>
+           </swiper>
+       </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'gallery',
+    data(){
+        return {
+            swiperOptions:{
+                pagination : '.swiper-pagination',
+                paginationType:'fraction',
+                observeParents: true,
+                observer: true,
+            },
+            imgs:[
+                'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg',
+                'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg'
+            ]
+        }
+    },
+    methods:{
+        closeImgs(){
+            this.$emit('close');
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .gallery>>> .swiper-container{
+        overflow:inherit;
+    }
+    .gallery{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        z-index:99;
+        background-color #000;
+        position: fixed;
+        top:0;
+        left:0;
+        right:0;
+        bottom:0;
+        .gallery-img{
+            width:100%;
+            height:0;
+            padding-bottom: 100%
+            .banner-img{
+                width:100%;
+            }
+            .swiper-pagination{
+                color:#fff;
+                bottom: -1rem;
+            }
+        }
+    }
+</style>
